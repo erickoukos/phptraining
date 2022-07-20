fd7390e79ebe3d1f58b2a2d2dec8e5437848fb10
erickoukos <erickoukos@gmail.com>
2022-07-20 12:46:55 +0300

    Worked on Outputs and Variables

--- 01_output.php
+++ 01_output.php
@@ -2,5 +2,63 @@
 /* -------- Output & Comments ------- */
 
 /*
-  A .php file running on a server, can output both HTML and PHP code. There are multiple functions that can be used to output data to the browser.
+  A .php file running on a server, can output both HTML and PHP code. 
+  There are multiple functions that can be used to output data to the browser.
 */
+// single line comment
+// echo - output strings, numbers, html, etc
+
+echo 'Hello World!';
+echo '<br/>';
+echo 123;
+echo '<br/>';
+echo '<h1> Welcome to PHP</h1>';
+
+// print - Similar to echo, but slower
+echo '<br/>';
+print 'Hello Erick';
+echo '<br/>';
+
+// print_r - gives more info - usually for arrays
+print_r("Hello");
+echo '<br/>';
+print_r([1, 2, 3]);
+
+// var_dump - even more info compared to print_r
+echo '<br/>';
+var_dump('Hello');
+echo '<br/>';
+
+$names = "Erick Ouko";
+var_dump($names);
+
+// escaping chars using backslash
+echo '<br/>';
+echo 'What\'s up';
+
+/*
+Multiple line comments
+are done
+like this
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PhP inside Html</title>
+</head>
+<body>
+  <p><?php echo 'This is php inside HTML5'; ?></p>
+
+  <br>
+
+  <?php 
+
+  echo 5 + 8;
+
+   ?>
+</body>
+</html>
--- 02_variables.php
+++ 02_variables.php
@@ -6,7 +6,7 @@
 /*
 - String - A string is a series of characters surrounded by quotes
 - Integer - Whole numbers
-- Float - Decimal numbers
+- Float - Decimal numbers 5.2
 - Boolean - true or false
 - Array - An array is a special variable, which can hold more than one value
 - Object - A class
@@ -19,6 +19,62 @@
 - Variables must be prefixed with $
 - Variables must start with a letter or the underscore character
 - variables can't start with a number
-- Variables can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+- Variables can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ ) 
 - Variables are case-sensitive ($name and $NAME are two different variables)
 */
+
+$name = 'John Smith';
+echo $name;
+echo '<br>';
+
+$has_children = false;
+var_dump($has_children);
+echo $has_children;
+echo '<br>';
+
+$age = 25;
+echo $age;
+echo '<br>';
+
+$my_height = 186.9;
+var_dump($my_height);
+
+// Adding Strings to variables
+
+// Using Double Quotes
+
+echo '<br>';
+echo "My name is $name and my age is $age with a height of $my_height cm.";
+echo '<br>';
+// Always do it this way
+echo "My name is {$name} and my age is {$age} with a height of {$my_height} cm.";
+
+echo '<br>';
+echo '<br>';
+
+// Concatenate incase of single quotes using . 
+echo 'My name is ' . $name . 'and my age is ' . $age . 'with a height of ' . $my_height . 'cm.';
+
+// Arithmetic operators + - / * %
+echo '<br>';
+echo 5 + 5; // 10
+echo '<br>';
+echo 10 - 6; // 4
+echo '<br>';
+echo 8 * 5; //40
+echo '<br>';
+echo 10 / 2; // 5
+echo '<br>';
+echo 7 % 2; // 1
+
+// Constants - cannot be changed.
+
+define('HOST', 'localhost');
+define('USER', 'root');
+
+echo '<br>';
+
+var_dump(HOST);
+echo '<br>';
+
+var_dump(USER);
