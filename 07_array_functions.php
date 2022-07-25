81b093946d13f5be6afc5b638f6ae27c7a192cc4
erickoukos <erickoukos@gmail.com>
2022-07-25 14:06:08 +0300

    New Training files

---
+++ 07_array_functions.php
@@ -0,0 +1,53 @@
+<?php
+/* --------- Array Functions -------- */
+
+/*
+  Functions to work with arrays
+  https://www.php.net/manual/en/ref.array.php
+*/
+
+
+
+// Get array length
+
+
+// Search array
+
+
+// Add to an array
+
+// Adds to the beginning
+
+// Remove from array
+ // Removes last
+ // Removes first
+// Remove specific element
+
+
+// Split into chunks of 2
+
+
+// Concatenate arrays
+
+
+// Combine arrays (Keys & values)
+
+
+// Array of keys
+
+
+// Flip keys with values
+
+
+// Create array of numbers with range()
+
+
+// Map through array and create a new one
+
+
+// Filter array
+
+
+// Array Reduce
+// "carry" holds the return value of the previous iteration
+
---
+++ 08_string_functions.php
@@ -0,0 +1,48 @@
+<?php
+/* ---------- String Functions -------- */
+
+/*
+  Functions to work with strings
+  https://www.php.net/manual/en/ref.strings.php
+*/
+
+
+
+// Get the length of a string
+
+
+// Find the position of the first occurrence of a substring in a string
+
+
+// Find the position of the last occurrence of a substring in a string
+
+
+// Reverse a string
+
+
+// Convert all characters to lowercase
+
+
+// Convert all characters to uppercase
+
+
+// Uppercase the first character of each word
+
+
+// String replace
+
+
+// Return portion of a string specified by the offset and length
+
+// Starts with
+
+
+// Ends with
+
+
+// HTML Entities
+
+
+// Formatted Strings - useful when you have outside data
+// Different specifiers for different data types
+ // float
